9358afe3af06052ce74d31324fd3a69b39e8d07e
Wilmer Lozano <wglozanoh@gmail.com>
2026-02-19 15:25:48 -0500

    chore: update execution scripts to include staging layer

--- 00_run_all.sql
+++ 00_run_all.sql
@@ -4,4 +4,6 @@
 
 :r .\sql\01_create_database.sql
 :r .\sql\02_create_schemas.sql
+:r .\sql\03_create_staging_tables.sql
+:r .\sql\04_load_staging_data.sql
 
--- run_project.bat
+++ run_project.bat
@@ -3,7 +3,9 @@ cd /d "%~dp0"
 
 echo Starting COVID19 LATAM Analytics setup...
 
-sqlcmd -S localhost -E -i 00_run_all.sql
+sqlcmd -S localhost -E ^
+-v DATA_PATH="%~dp0dataset" ^
+-i 00_run_all.sql
 
 echo Setup completed successfully.
 pause
